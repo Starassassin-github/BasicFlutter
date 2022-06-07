1083496312ce9b4db434b479bffcf542f5a63cae
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-06-07 12:00:12 +0700

    Update main.dart

--- my_fist_basic_flutter/lib/main.dart
+++ my_fist_basic_flutter/lib/main.dart
@@ -5,23 +5,56 @@ void main() {
   runApp(app);
 }
 
+// สร้าง widget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Flutter ครับ"),
-        ),
-        body: Center(
-          child: Text(
-            "Hello Everyone",
-            style: TextStyle(fontSize: 30, color: Colors.pink),
-          ),
-        ),
-      ),
+      home: MyHomePage(),
       theme: ThemeData(primarySwatch: Colors.deepPurple),
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int number = 0; // การสร้าง state
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Flutter ครับ"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("กดปุ่มเพื่อเพิ่มจำนวนตัวเลข"),
+              Text(
+                "$number",
+                style: TextStyle(fontSize: 60),
+              )
+            ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNumber,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void addNumber(){
+    setState(() {
+        number++;
+    });
+  }
+
+}
+
