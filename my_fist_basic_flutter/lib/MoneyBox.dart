69fe8ee2ff581adcae44c21cf500b3f2191485b7
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-06-09 13:33:50 +0700

    finish this course
    knowing api http convert json to dart
    null future builder snapshot

--- my_fist_basic_flutter/lib/MoneyBox.dart
+++ my_fist_basic_flutter/lib/MoneyBox.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
-// สร้่าง Container ต้นแบบเอาไว้
+// สร้าง container ต้นแบบเอาไว้
 class MoneyBox extends StatelessWidget {
-  late String title; // ชื่อหมวดหมู่ รายการ
-  late double amount; // จำนวนเงิน
-  late Color color; // สีของกล่อง
-  late double size; // ขนาดของกล่อง
 
-  MoneyBox(this.title, this.amount, this.color, this.size);
+  String title;//ชื่อสกุลเงิน
+  double amount;//จำนวนเงิน
+  Color color;//สีของกล่อง
+  double size;//ขนาดของกล่อง
+
+
+  MoneyBox(this.title,this.amount,this.color,this.size);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8.0),
-      decoration:
-          BoxDecoration(color: color, borderRadius: BorderRadius.circular(10)),
+      padding: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.circular(10)),
       height: size,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -23,15 +24,18 @@ class MoneyBox extends StatelessWidget {
           Text(
             title,
             style: TextStyle(
-                fontSize: 40, color: Colors.white, fontWeight: FontWeight.bold),
+                fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
           ),
           Expanded(
-              child: Text(
-            '${NumberFormat("#,###.##").format(amount)}',
-            style: TextStyle(
-                fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.right,
-          ))
+            child: Text(
+              '${NumberFormat("#,###.##").format(amount)}',
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.right,
+            ),
+          )
         ],
       ),
     );
--- my_fist_basic_flutter/lib/main.dart
+++ my_fist_basic_flutter/lib/main.dart
@@ -1,12 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'FoodMenu.dart';
+import 'package:http/http.dart' as http;
+import 'ExchangeRate.dart';
 import 'MoneyBox.dart';
+
 void main() {
-  
-  var app = MyApp();
-  runApp(app);
+  runApp(MyApp());
 }
 
 // สร้าง widget
@@ -16,52 +14,65 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "My App",
       home: MyHomePage(),
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.purple),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-  int number = 0;
+  
+  late ExchangeRate _dataFromAPI;
+  
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    print("init State");
+    getExchangeRate();
   }
-  // แสดงผลข้อมูล
+  Future <ExchangeRate> getExchangeRate() async{
+     var url = Uri.parse("https://api.exchangerate-api.com/v4/latest/THB");
+     var response=await http.get(url);
+     _dataFromAPI=exchangeRateFromJson(response.body);//json => dart object
+     return _dataFromAPI;
+  }
   @override
   Widget build(BuildContext context) {
-    print("Call Build");
     return Scaffold(
         appBar: AppBar(
-          title: Text(
-            "บัญชีของฉัน",
-            style: TextStyle(
-                fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
-          ),
+          title: Text("แปลงสกุลเงิน",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
         ),
-        body: Column(
-          children: [
-            Text(number.toString(),style: TextStyle(fontSize: 30),)
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          setState(() {
-            number++;
-          });
-        },
-        child: Icon(Icons.add)
-        ,),);
+        body:FutureBuilder(
+          future: getExchangeRate(),
+          builder: (BuildContext context,AsyncSnapshot<dynamic> snapshot){
+            //ดึงข้อมูลจาก getExchangeRate มาครบเรียบร้อยจะให้ทำอะไร  
+            if(snapshot.connectionState == ConnectionState.done){
+                var result = snapshot.data;
+                double amount=10000;
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                      children: [
+                        MoneyBox("สกุลเงิน (THB)",amount,Colors.lightBlue,150),
+                        SizedBox(height: 5,),
+                        MoneyBox("USD", amount*result.rates["USD"], Colors.green, 100),
+                        SizedBox(height: 5,),
+                        MoneyBox("EUR", amount*result.rates["EUR"], Colors.red, 100),
+                        SizedBox(height: 5,),
+                        MoneyBox("GBP", amount*result.rates["GBP"], Colors.pink, 100),
+                         SizedBox(height: 5,),
+                        MoneyBox("JPY", amount*result.rates["JPY"], Colors.orange, 100),
+                      ],
+                  ),
+                );
+            }
+            return LinearProgressIndicator();
+        },)
+        );
   }
 }
-
-
-
